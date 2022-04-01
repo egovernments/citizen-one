fe2508150db5b7dff222df848f2f2c26ab958e81
RamkrishnaEgovt <ramkrishna.sahoo@egovernments.org>
2022-04-01 12:23:44 +0530

    CT-7:Added dateFormats and Custom Exception formats

---
+++ egov_widgets/lib/utils/custom_exception.dart
@@ -0,0 +1,15 @@
+import 'models.dart';
+
+class CustomException implements Exception {
+  final String message;
+  final int statusCode;
+  final ExceptionType exceptionType;
+  final String? code;
+
+  CustomException(this.message, this.statusCode, this.exceptionType,
+      {this.code});
+
+  String toString() {
+    return "$message";
+  }
+}
